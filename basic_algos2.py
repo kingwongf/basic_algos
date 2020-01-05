25ad3363b229e40e7f46dd4673d2136a8f5c6515
King F. Wong <kingf.wong@kings-mbp.mynet>
2020-01-05 22:44:59 +0000

    insertion included

--- basic_algos2.py
+++ basic_algos2.py
@@ -52,14 +52,32 @@ def bubble_sort(arr):
         if swapped is False:
             return arr
 
+
+def insertionSort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+
+        key = arr[i]
+        print("key %s"%key)
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+            print(arr, "inside while loop j %s"%j)
+        arr[j + 1] = key
+        print(arr, "inside for loop i %s"%i)
+    return arr
+
 def binomia_coeff(n,k):
     res = 1
     for i in range(n):
         res*=i
         res/=()
 
-
-print(merge_sort([9,8,7,6,4,3,3,1]))
-print(bubble_sort([9,8,7,6,4,3,3,1]))
-
-print(binary_search([1,2,3,4,7,8,9],4))
+print(insertionSort([7,9,8,7.4]))
+#print(merge_sort([9,8,7,6,4,3,3,1]))
+#print(bubble_sort([9,8,7,6,4,3,3,1]))
+#print(binary_search([1,2,3,4,7,8,9],4))
